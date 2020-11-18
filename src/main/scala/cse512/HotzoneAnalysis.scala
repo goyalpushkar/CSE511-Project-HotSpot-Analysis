2aade6b32baf310bdca08351ca8f0b8a7e42afe8
Pushkar Goyal <goyalpushkar@gmail.com>
2020-11-18 02:01:38 -0500

    Logging and HotZone code added

--- src/main/scala/cse512/HotzoneAnalysis.scala
+++ src/main/scala/cse512/HotzoneAnalysis.scala
@@ -14,24 +14,40 @@ object HotzoneAnalysis {
 
     var pointDf = spark.read.format("com.databricks.spark.csv").option("delimiter",";").option("header","false").load(pointPath);
     pointDf.createOrReplaceTempView("point")
-
+    
+    logger.info("Point Data Set")
+    pointDf.show(10) //Pushkar
+    
     // Parse point data formats
     spark.udf.register("trim",(string : String)=>(string.replace("(", "").replace(")", "")))
     pointDf = spark.sql("select trim(_c5) as _c5 from point")
     pointDf.createOrReplaceTempView("point")
+    
+    logger.info("Point Data Set after trimming")
+    pointDf.show(10) //Pushkar 
 
     // Load rectangle data
     val rectangleDf = spark.read.format("com.databricks.spark.csv").option("delimiter","\t").option("header","false").load(rectanglePath);
     rectangleDf.createOrReplaceTempView("rectangle")
-
+    
+    logger.info("Rectangle Data Set")
+    rectangleDf.show(10)  //Pushkar
+    
     // Join two datasets
     spark.udf.register("ST_Contains",(queryRectangle:String, pointString:String)=>(HotzoneUtils.ST_Contains(queryRectangle, pointString)))
     val joinDf = spark.sql("select rectangle._c0 as rectangle, point._c5 as point from rectangle,point where ST_Contains(rectangle._c0,point._c5)")
     joinDf.createOrReplaceTempView("joinResult")
 
+    logger.info("Joined Data Set")
+    joinDf.show(10)
+    
     // YOU NEED TO CHANGE THIS PART
-
-    return joinDf // YOU NEED TO CHANGE THIS PART
+    //Count number of points in each rectangle by grouping the join result on rectangle
+    val hotzone = spark.sql("""SELECT rectangle, COUNT(point) no_of_points FROM joinResult GROUP BY rectangle""")
+    logger.info("HotZone Data Set")
+    hotzone.show(10)
+    
+    return hotzone  //joinDf // YOU NEED TO CHANGE THIS PART
   }
 
 }
